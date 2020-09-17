1383dda515208893896b0fbdb98a9837ab82a085
liaowenqiang828 <18840965351@163.com>
2020-09-17 17:07:40 +0800

    [Wenqiang Liao] feat: solve problem 3 method 1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,13 +1,15 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bar {
 
     private Foo foo;
-
-    public Bar(Foo foo) {
+    @Autowired
+    public Bar(@Lazy Foo foo) {
         this.foo = foo;
     }
 
